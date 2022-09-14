49fa3a09fbc4bee6f2a8653a64156c6822fd989e
EmilWallett <mhdisy0@gmail.com>
2022-09-14 11:43:25 +0200

    Add project files.

---
+++ Uppgift 2.11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift_2_11
+{
+    class program
+    {
+        static void Main(string[]arg)
+        {
+            Console.WriteLine("HUR LÅNGT HAR ELIN HOPPAT.");
+            int ELIN = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("HUR LÅNGT HAR ELMA HOPPAT.");
+            int ELMA = int.Parse(Console.ReadLine());
+            int RESULTAT = ELIN - ELMA;
+            Console.WriteLine("ELIN HAR HOPPAT " + RESULTAT + " METER MER ÄN ELMA");
+        }
+    }
+}
